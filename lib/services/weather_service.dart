75e8476f3878195b783ef140bf0db5a6345b730c
MK <103623651+MangeshKakade@users.noreply.github.com>
2022-12-10 23:25:54 +0530

    Add files via upload
        # weather_app
    
    A new Flutter project.
    
    ## Getting Started
    
    This project is a starting point for a Flutter application.
    
    A few resources to get you started if this is your first Flutter project:
    
    - [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
    - [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
    
    For help getting started with Flutter development, view the
    [online documentation](https://docs.flutter.dev/), which offers tutorials,
    samples, guidance on mobile development, and a full API reference.

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:weather_app/models/weather_api.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService{
+  Future<Weather> getWeatherData() async {
+    final uri = Uri.parse('http://api.weatherapi.com/v1/forecast.json?key=9374b885f9824b808cc113544220912&q=$city&days=1&aqi=no&alerts=no');
+
+    final response = await http.get(uri);
+    if(response.statusCode == 200){
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed');
+    }
+  }
+}
+String city = 'Los Angeles';
